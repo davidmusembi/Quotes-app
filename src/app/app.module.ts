72f3dfed33bcdacd2d947372cd8016a3dec4d82d
David Musembi <davidmusembi02@gmail.com>
2022-04-09 22:07:55 +0300

    Add Google font on stle.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuotesDetailsComponent } from './components/quotes-details/quotes-details.component';
+import { QuotesFormComponent } from './components/quotes-form/quotes-form.component';
+import { HighlightQuoteDirective } from './directives/highlight-quote.directive';
+import { TimeQuotePipe } from './pipes/time-quote.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailsComponent,
+    QuotesFormComponent,
+    HighlightQuoteDirective,
+    TimeQuotePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
