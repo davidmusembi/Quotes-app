4c4d6593dc77c5ff7ca81347fc69d5d2a4cf8b93
David Musembi <davidmusembi02@gmail.com>
2022-04-09 19:46:29 +0300

    Add the quotes-form

--- src/app/components/quotes-form/quotes-form.component.ts
+++ src/app/components/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quotes } from 'src/app/models/quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+ newQuotes = new Quotes ("","","",new Date(),0,0);
+    @Output() addQuotes = new EventEmitter < Quotes > ();
 
+    // function that creates a new quote array
+
+    submittingQuote(){
+
+        this.addQuotes.emit(this.newQuotes);
+        this.newQuotes = new Quotes("","","",new Date(),0,0)
+    }
+    
   constructor() { }
 
   ngOnInit(): void {
